c347882cc7c48090f9e9ecbc05fbe124a0bd095e
ChrisCoy <christopherleegs@outlook.com>
2023-08-20 20:23:36 -0300

    feat: challenge finished

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list, acc \\ 0)
+
+  def call([], acc), do: acc
+
+  def call(list, acc) do
+    [_head | tail] = list
+    call(tail, acc + 1)
   end
 end
